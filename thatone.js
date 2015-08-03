fbae1388ce5f808083c461dba5d6aa0db4408aa7
jdlawrence <jamil.lawrence@gmail.com>
2015-08-03 12:37:46 -0700

    Added gitignore and angular

--- thatone.js
+++ thatone.js
@@ -4,6 +4,6 @@ var TIME = 10;
 var RATE = 8;
 
 // Calculate interest with given parameters
-ENDING = Math.roundSTARTING * Math.pow(1 + RATE / 100, TIME);
+ENDING = Math.round(STARTING * Math.pow(1 + RATE / 100, TIME), 2);
 
 console.log('STARTING', STARTING, 'ENDING', ENDING);
