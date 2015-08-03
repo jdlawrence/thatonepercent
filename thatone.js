c3b87a507c1465c039eeecaec268673de2fdfa10
jdlawrence <jamil.lawrence@gmail.com>
2015-08-03 16:35:33 -0700

    All inputs are now working and growth calculation is correctly calculated based off of inputs!

--- thatone.js
+++ thatone.js
@@ -1,22 +1,27 @@
 var starting = 100;
-var ending = 0;
-var timePeriod = 10;
-var rate = 8;
+var ending = 110;
+var timePeriod = 1;
+var rate = 10.0;
 
 // Calculate interest with given parameters
-ending = Number(starting * Math.pow(1 + rate / 100, timePeriod)).toFixed(2);
+// ending = Number(starting * Math.pow(1 + rate / 100, timePeriod)).toFixed(2);
 
 var app = angular.module('thatone', []);
 
 app.controller('inputCtrl', ['$scope', function($scope) {
   $scope.inputs = {
     starting: starting,
-    ending: ending,
     timePeriod: timePeriod,
     rate: rate
   };
-  
-  // console.log('input.value', $scope.inputs);
 
+
+  // 'ending' value is calculated any time there's a change on any of the input values
+  // 
+  $scope.$watchCollection('inputs', calcEnding);
+  function calcEnding(inputs) {
+    $scope.ending = Number($scope.inputs.starting * Math.pow(1 + $scope.inputs.rate / 100, $scope.inputs.timePeriod)).toFixed(2);
+  }
 
 }]);
+
