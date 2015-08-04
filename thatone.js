119bf848f59dca0b8b42583c901b918da7d44938
jdlawrence <jamil.lawrence@gmail.com>
2015-08-04 12:49:20 -0700

    Push before trying removing watch from controller and moving to directive

--- thatone.js
+++ thatone.js
@@ -3,7 +3,6 @@ var ending = 110;
 var timePeriod = 1;
 var rate = 10.0;
 var numBuckets = 20;
-var buckets = [];
 
 // Calculate interest with given parameters
 // ending = Number(starting * Math.pow(1 + rate / 100, timePeriod)).toFixed(2);
@@ -16,45 +15,45 @@ app.controller('inputCtrl', ['$scope', function($scope) {
     timePeriod: timePeriod,
     rate: rate
   };
-  $scope.buckets = [];
 
   // 'ending' value is calculated any time there's a change on any of the input values
   $scope.$watchCollection('inputs', calcEnding);
   function calcEnding(inputs) {
     $scope.ending = Number($scope.inputs.starting * Math.pow(1 + $scope.inputs.rate / 100, $scope.inputs.timePeriod)).toFixed(2);
-    $scope.dataToGraph = [];
+    $scope.endingAmounts = [];
     var timeXValue = 0;
     var amountYValue = 0;
 
     // Generate an array of ending values based on time divided into buckets
     for (var i = 0; i < numBuckets; i++) {
-      time = Number($scope.inputs.timePeriod / numBuckets * i).toFixed(2);
+      time = Number($scope.inputs.timePeriod / numBuckets * (i+1)).toFixed(2);
       amountYValue = Number($scope.inputs.starting * Math.pow(1 + $scope.inputs.rate / 100, time)).toFixed(2);
-      $scope.dataToGraph.push({
-        time : time,
-        amount: amountYValue
-      });
+      $scope.endingAmounts.push(amountYValue);
     }
-    console.log('bu************ckets', $scope.dataToGraph);
+    // console.log('timeAmount', $scope.endingAmounts);
 
   }
 
 }]);
 
-app.directive("expGraph", function($window) {
+app.directive("expGraph", function($parse, $window) {
   return{
-    restrict: "EA",
+    restrict: "E",
     template: "<svg width='850' height='200'></svg>",
+    scope: false,
     link: function(scope, elem, attrs){
 
-      // var endingAmounts=scope[attrs.chartData];
-      // var padding = 20;
-      // var pathClass = "path";
-      // var xScale, yScale, xAxisGen, yAxisGen, lineFun;
+      console.log('scope', scope);  
+      var exp = $parse(attrs.chartData);
+      // console.log('scope', scope);
 
-      // var d3 = $window.d3;
-      // var rawSvg = elem.find("svg")[0];
-      // var svg = d3.select(rawSvg);
-    }
-  };
+      var dataToPlot = scope; // Collect data from scope in controller
+      // console.log('dataToPlot', dataToPlot);
+      
+
+      // scope.$watchCollection(exp, function(newVal, oldVal){
+      //   console.log(dataToPlot);
+      // });
+}
+};
 });
